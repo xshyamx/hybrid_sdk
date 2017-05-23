72255d5a5e076f6ffae20d2aff998eaa78f71e23
Jebin B V <jebin@avaamo.com>
2017-05-23 15:30:19 +0530

    cleaned up and added documentation

--- sample.js
+++ sample.js
@@ -1,15 +1,12 @@
-const Avaamo = require("./index"),
-	UUID = require("uuid");
- // const bot_uuid = "9235844f-78ee-45ea-b285-5367a71f2808",
- //  	access_token = "O7wyIr9i44f43mMvW3lvBqtNAUeaw0-D",
-const bot_uuid = "36a39928-7a82-4450-9fc7-760207ab6b71",
-	access_token = "3RnVNLZjLh55jXDcm6wU6X5bXZD8t72_";
+const Avaamo = require("./index");
+const bot_uuid = "<YOUR_BOT_UUID>",
+	access_token = "<YOUR_BOT_ACCESS_TOKEN>";
 
 new Avaamo(bot_uuid, access_token, {
 	create_ticket: (response) => {
-		console.log("Request from bot engine", JSON.stringify(response));
+		console.log("Request from designer", JSON.stringify(response));
 		return {
-			ticket_id: UUID.v4()
+			ticket_id: "My Ticket ID"
 		};
 	},
 	edit_ticket: (response) => {
